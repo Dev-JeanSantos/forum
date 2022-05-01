9c4e1fd1f341fad125656f85f632ef65f231310a
Jean Santos <jeancbsan@gmail.com>
2022-04-30 22:21:37 -0300

    implementação do AutenticaçãoService e classe de perfis com a logica de acesso por login com criptografia de senha

--- src/main/java/br/com/alura/forum/modelos/Usuario.java
+++ src/main/java/br/com/alura/forum/modelos/Usuario.java
@@ -1,16 +1,24 @@
 package br.com.alura.forum.modelos;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
-public class Usuario {
+public class Usuario implements UserDetails {
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         private Long id;
         private String nome;
         private String email;
         private String senha;
+        @ManyToMany(fetch = FetchType.EAGER)
+        private List<Perfil> perfis = new ArrayList<>();
 
     public Usuario(Long id, String nome, String email, String senha) {
         this.id = id;
@@ -50,5 +58,40 @@ public class Usuario {
     @Override
     public int hashCode() {
         return Objects.hash(getId(), getNome());
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.perfis;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$ub6Fk/oG5eLayU.SrQv5QuBWKRVrdXyGTiS8ukaWrapCGIRlv1.AW');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
